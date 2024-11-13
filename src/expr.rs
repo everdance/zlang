7d132441fafe472101435cb7f37ae2ac5e8df6e3
xiaolei <tristones.liu@gmail.com>
2024-11-13 16:20:47 -0500

    define expr and stmt

--- src/expr.rs
+++ src/expr.rs
@@ -1,4 +1,4 @@
-use crate::token::TokenKind;
+use crate::token::{Token, TokenKind};
 
 pub enum ExprType {
     Literal(TokenKind),
@@ -20,4 +20,41 @@ pub struct Expr {
     pub left: Option<Box<Expr>>,
     pub right: Option<Box<Expr>>,
     pub list: Option<Vec<Expr>>,
+}
+
+pub struct IfStmt {
+    pub cond: Expr,
+    pub thens: Vec<Stmt>,
+    pub elses: Option<Vec<Stmt>>,
+}
+
+pub struct BlkStmt {
+    pub list: Vec<Stmt>,
+}
+
+pub struct WhileStmt {
+    pub cond: Expr,
+    pub list: Vec<Stmt>,
+}
+
+pub struct FunStmt {
+    pub name: Token,
+    pub params: Vec<Token>,
+    pub body: Vec<Stmt>,
+}
+
+pub struct ClassStmt {
+    pub name: Token,
+    pub methods: Vec<FunStmt>,
+}
+
+pub enum Stmt {
+    Expr(Expr),
+    Var(Token, Expr),
+    Return(Token, Expr),
+    If(IfStmt),
+    Block(BlkStmt),
+    While(WhileStmt),
+    Fun(FunStmt),
+    Class(ClassStmt),
 }
