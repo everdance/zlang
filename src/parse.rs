7bea6c62d3ed3944de97bf0c673b8a591857c42c
xiaolei <tristones.liu@gmail.com>
2024-11-13 17:35:17 -0500

    parse

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,10 @@
-use crate::expr::Expr;
+use crate::expr::Stmt;
 use crate::token::Token;
 
 pub struct Parser;
 
 impl Parser {
-    pub fn parse(tokens: Vec<Token>) -> Result<Expr, String> {
+    pub fn parse(tokens: Vec<Token>) -> Result<Vec<Stmt>, String> {
         Err("to implement".to_string())
     }
 }
