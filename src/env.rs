9ec70f6000f9e5a3910b24312178797e11aef6e1
xiaolei <tristones.liu@gmail.com>
2024-12-09 16:24:27 -0500

    refactor env

--- src/env.rs
+++ src/env.rs
@@ -1,50 +1,57 @@
 use crate::expr;
-use std::{collections::HashMap, rc::Rc};
-
-#[derive(Debug)]
-pub struct Object<'a> {
-    class: String,
-    props: HashMap<String, Value<'a>>,
-}
+use std::collections::HashMap;
+use std::rc::Rc;
 
 #[derive(Debug, Clone)]
-pub enum Value<'a> {
+pub enum Value {
     Nil,
     Str(String),
     Num(f64),
     Bool(bool),
-    Fun(&'a expr::Fun),
-    Class(HashMap<String, &'a expr::Fun>),
-    Object(Rc<Object<'a>>),
+    Fun(Rc<expr::Fun>),
+    Class(Rc<HashMap<String, expr::Fun>>),
+    Object(String, Rc<HashMap<String, Value>>),
 }
 
-pub fn new(parent: Option<Rc<Environment>>) -> Environment {
-    Environment {
-        parent,
-        map: HashMap::new(),
+pub struct Environments {
+    stack: Vec<HashMap<String, Value>>,
+}
+
+pub fn new() -> Environments {
+    Environments { stack: vec![] }
+}
+
+impl Environments {
+    pub fn new_stack(&mut self, depth: usize) -> Environment {
+        self.stack.truncate(depth - 1);
+        self.stack.push(HashMap::new());
+        Environment {
+            stack: &mut self.stack,
+            depth,
+        }
     }
 }
 
 pub struct Environment<'a> {
-    parent: Option<Rc<Environment<'a>>>,
-    map: HashMap<String, Value<'a>>,
+    stack: &'a mut [HashMap<String, Value>],
+    depth: usize,
 }
 
 impl<'a> Environment<'a> {
-    pub fn get(&self, id: &str) -> Option<&Value> {
-        match self.map.get(id) {
-            Some(val) => Some(val),
-            None => {
-                if let Some(penv) = &self.parent {
-                    penv.get(id)
-                } else {
-                    None
-                }
-            }
-        }
+    pub fn depth(&self) -> usize {
+        self.depth
     }
 
-    pub fn set(&mut self, id: String, val: Value<'a>) {
-        self.map.insert(id, val);
+    pub fn get(&self, id: &str) -> Option<&Value> {
+        for ev in self.stack.iter().rev() {
+            if let Some(val) = ev.get(id) {
+                return Some(val);
+            }
+        }
+        None
+    }
+
+    pub fn set(&mut self, id: String, val: Value) {
+        self.stack.last_mut().unwrap().insert(id, val);
     }
 }
--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,6 @@
-use crate::env::{self, Environment, Value};
+use std::collections::HashMap;
+
+use crate::env::{self, Environment, Environments, Value};
 use crate::expr;
 use crate::expr::{
     Expr,
@@ -10,169 +12,186 @@ use crate::token::Kind;
 pub struct Eval;
 
 impl Eval {
-    pub fn exec(stmts: &[Stmt]) -> () {}
-}
-
-fn eval_expr<'a>(exp: &Expr, ev: &'a mut Environment<'a>) -> Value<'a> {
-    match exp.kind {
-        Literal => match exp.token.kind.clone() {
-            Kind::NumLiteral(x) => Value::Num(x),
-            Kind::StrLiteral(x) => Value::Str(x),
-            Kind::True => Value::Bool(true),
-            Kind::False => Value::Bool(false),
-            Kind::Nil => Value::Nil,
-            _ => unreachable!(),
-        },
-        Identifier => {
-            if let Some(val) = ev.get(&exp.token.val()) {
-                val.clone()
-            } else {
-                panic!("undefined variable")
-            }
+    pub fn exec(stmts: &[Stmt]) -> () {
+        let mut eval = evaluator { envs: env::new() };
+        let mut global = eval.new_env(1);
+        for stmt in stmts.iter() {
+            eval.eval_stmt(stmt, &mut global);
         }
-        Unary => match eval_expr(exp.left.as_deref().unwrap(), ev) {
-            Value::Bool(x) => Value::Bool(!x),
-            Value::Num(x) => Value::Num(-x),
-            _ => panic!("unexpected value"),
-        },
-        Binary => {
-            let left = eval_expr(exp.left.as_deref().unwrap(), ev);
-            let right = eval_expr(exp.right.as_deref().unwrap(), ev);
-            match (left, right) {
-                (Value::Num(x), Value::Num(y)) => match exp.token.kind {
-                    Kind::Star => Value::Num(x * y),
-                    Kind::Slash => Value::Num(x / y),
-                    Kind::Plus => Value::Num(x + y),
-                    Kind::Minus => Value::Num(x - y),
-                    Kind::Greater => Value::Bool(x > y),
-                    Kind::GreaterEqual => Value::Bool(x >= y),
-                    Kind::Less => Value::Bool(x < y),
-                    Kind::LessEqual => Value::Bool(x <= y),
-                    _ => panic!("unexpected binary token"),
-                },
-                (_, _) => panic!("unexpected value for binary"),
-            }
-        }
-        Logical => {
-            let left = eval_expr(exp.left.as_deref().unwrap(), ev);
-            let right = eval_expr(exp.right.as_deref().unwrap(), ev);
-            match (left, right) {
-                (Value::Bool(x), Value::Bool(y)) => {
-                    if exp.token.kind == Kind::Or {
-                        Value::Bool(x || y)
-                    } else if exp.token.kind == Kind::And {
-                        Value::Bool(x && y)
-                    } else {
-                        panic!("unexpected logic token")
-                    }
-                }
-                (_, _) => panic!("unexpected value for logic"),
-            }
-        }
-        Grouping => eval_expr(exp.left.as_deref().unwrap(), ev),
-        Call => eval_call(exp.left.as_deref().unwrap(), exp.list.as_ref().unwrap(), ev),
-        Assign => eval_assign(exp, ev),
-        Get => eval_get(exp, ev),
-        _ => Value::Nil,
     }
 }
 
-fn eval_call<'a>(callee: &Expr, params: &[Expr], ev: &'a mut Environment<'a>) -> Value<'a> {
-    // find func definition
-    let method = match callee.kind {
-        ExprType::Identifier => ev.get(&callee.token.val()).unwrap(),
-        ExprType::Get => &eval_get(callee, ev),
-        _ => unreachable!(),
-    };
+struct evaluator {
+    envs: Environments,
+}
 
-    let mut closure = env::new(Some(ev));
-
-    if let Value::Fun(fun) = method {
-        for (index, param) in fun.params.iter().enumerate() {
-            closure.set(param.val(), eval_expr(params.get(index).unwrap(), ev));
-        }
-        for stmt in fun.body.iter() {
-            eval_stmt(stmt, &closure);
-        }
-    } else {
-        panic!("unexpected value for function:{:?}", method)
+impl evaluator {
+    fn new_env(&mut self, current: usize) -> Environment {
+        return self.envs.new_stack(current);
     }
-    Value::Nil
-}
 
-fn eval_get<'a>(exp: &Expr, ev: &'a mut Environment<'a>) -> Value<'a> {
-    Value::Nil
-}
-
-fn eval_assign<'a>(exp: &Expr, ev: &'a mut Environment<'a>) -> Value<'a> {
-    Value::Nil
-}
-
-fn eval_stmt<'a>(stmt: &expr::Stmt, ev: &'a mut Environment<'a>) -> Value<'a> {
-    match stmt {
-        Stmt::Expr(exp) => eval_expr(exp, ev),
-        Stmt::Var(name, exp) => {
-            let val = match exp {
-                Some(epr) => eval_expr(epr, ev),
-                _ => Value::Nil,
-            };
-            ev.set(name.val(), val);
-            Value::Nil
-        }
-        Stmt::Return(epr) => Value::Nil, //TODO
-        Stmt::If(cond, ifstmt, elsest) => match eval_expr(cond, ev) {
-            Value::Bool(true) => {
-                for st in ifstmt.iter() {
-                    eval_stmt(st, ev);
-                }
+    fn eval_stmt(&mut self, stmt: &expr::Stmt, ev: &mut Environment) -> Value {
+        match stmt {
+            Stmt::Expr(exp) => self.eval_expr(exp, ev),
+            Stmt::Var(name, exp) => {
+                let val = match exp {
+                    Some(epr) => self.eval_expr(epr, ev),
+                    _ => Value::Nil,
+                };
+                ev.set(name.val(), val);
                 Value::Nil
             }
-            Value::Bool(false) => {
-                if let Some(stmts) = elsest {
-                    for st in stmts.iter() {
-                        eval_stmt(st, ev);
+            Stmt::Return(epr) => Value::Nil, //TODO
+            Stmt::If(cond, ifstmt, elsest) => match self.eval_expr(cond, ev) {
+                Value::Bool(true) => {
+                    for st in ifstmt.iter() {
+                        self.eval_stmt(st, ev);
                     }
+                    Value::Nil
                 }
-                Value::Nil
-            }
-            _ => unreachable!(),
-        },
-        Stmt::While(cond, stmts) => {
-            loop {
-                match eval_expr(cond, ev) {
-                    Value::Bool(true) => (),
-                    _ => break,
+                Value::Bool(false) => {
+                    if let Some(stmts) = elsest {
+                        for st in stmts.iter() {
+                            self.eval_stmt(st, ev);
+                        }
+                    }
+                    Value::Nil
                 }
-                for stmt in stmts.iter() {
-                    eval_stmt(stmt, ev);
-                }
-            }
-            Value::Nil
-        }
-        Stmt::For(stmt) => {
-            if let Some(st) = &stmt.var {
-                eval_stmt(st, ev);
-            }
-            loop {
-                if let Some(cond) = &stmt.cond {
-                    match eval_stmt(cond, ev) {
+                _ => unreachable!(),
+            },
+            Stmt::While(cond, stmts) => {
+                loop {
+                    match self.eval_expr(cond, ev) {
                         Value::Bool(true) => (),
                         _ => break,
                     }
+                    for stmt in stmts.iter() {
+                        self.eval_stmt(stmt, ev);
+                    }
                 }
-                for stmt in stmt.body.iter() {
-                    eval_stmt(stmt, ev);
+                Value::Nil
+            }
+            Stmt::For(stmt) => {
+                if let Some(st) = &stmt.var {
+                    self.eval_stmt(st, ev);
+                }
+                loop {
+                    if let Some(cond) = &stmt.cond {
+                        match self.eval_stmt(cond, ev) {
+                            Value::Bool(true) => (),
+                            _ => break,
+                        }
+                    }
+                    for stmt in stmt.body.iter() {
+                        self.eval_stmt(stmt, ev);
+                    }
+                }
+                Value::Nil
+            }
+            Stmt::Block(stmts) => {
+                for stmt in stmts.iter() {
+                    self.eval_stmt(stmt, ev);
+                }
+                Value::Nil
+            }
+            Stmt::Fun(fun) => Value::Nil,
+            Stmt::Class(cls) => Value::Nil,
+        }
+    }
+
+    fn eval_expr(&mut self, exp: &Expr, ev: &mut Environment) -> Value {
+        match exp.kind {
+            Literal => match exp.token.kind.clone() {
+                Kind::NumLiteral(x) => Value::Num(x),
+                Kind::StrLiteral(x) => Value::Str(x),
+                Kind::True => Value::Bool(true),
+                Kind::False => Value::Bool(false),
+                Kind::Nil => Value::Nil,
+                _ => unreachable!(),
+            },
+            Identifier => {
+                if let Some(val) = ev.get(&exp.token.val()) {
+                    val.clone()
+                } else {
+                    panic!("undefined variable")
                 }
             }
-            Value::Nil
-        }
-        Stmt::Block(stmts) => {
-            for stmt in stmts.iter() {
-                eval_stmt(stmt, ev);
+            Unary => match self.eval_expr(exp.left.as_deref().unwrap(), ev) {
+                Value::Bool(x) => Value::Bool(!x),
+                Value::Num(x) => Value::Num(-x),
+                _ => panic!("unexpected value"),
+            },
+            Binary => {
+                let left = self.eval_expr(exp.left.as_deref().unwrap(), ev);
+                let right = self.eval_expr(exp.right.as_deref().unwrap(), ev);
+                match (left, right) {
+                    (Value::Num(x), Value::Num(y)) => match exp.token.kind {
+                        Kind::Star => Value::Num(x * y),
+                        Kind::Slash => Value::Num(x / y),
+                        Kind::Plus => Value::Num(x + y),
+                        Kind::Minus => Value::Num(x - y),
+                        Kind::Greater => Value::Bool(x > y),
+                        Kind::GreaterEqual => Value::Bool(x >= y),
+                        Kind::Less => Value::Bool(x < y),
+                        Kind::LessEqual => Value::Bool(x <= y),
+                        _ => panic!("unexpected binary token"),
+                    },
+                    (_, _) => panic!("unexpected value for binary"),
+                }
             }
-            Value::Nil
+            Logical => {
+                let left = self.eval_expr(exp.left.as_deref().unwrap(), ev);
+                let right = self.eval_expr(exp.right.as_deref().unwrap(), ev);
+                match (left, right) {
+                    (Value::Bool(x), Value::Bool(y)) => {
+                        if exp.token.kind == Kind::Or {
+                            Value::Bool(x || y)
+                        } else if exp.token.kind == Kind::And {
+                            Value::Bool(x && y)
+                        } else {
+                            panic!("unexpected logic token")
+                        }
+                    }
+                    (_, _) => panic!("unexpected value for logic"),
+                }
+            }
+            Grouping => self.eval_expr(exp.left.as_deref().unwrap(), ev),
+            Call => self.eval_call(exp.left.as_deref().unwrap(), exp.list.as_ref().unwrap(), ev),
+            Assign => self.eval_assign(exp, ev),
+            Get => self.eval_get(exp, ev),
+            _ => Value::Nil,
         }
-        Stmt::Fun(fun) => Value::Nil,
-        Stmt::Class(cls) => Value::Nil,
+    }
+
+    fn eval_call(&mut self, callee: &Expr, params: &[Expr], ev: &mut Environment) -> Value {
+        // find func definition
+        let method = match callee.kind {
+            ExprType::Identifier => ev.get(&callee.token.val()).unwrap().clone(),
+            ExprType::Get => self.eval_get(callee, ev),
+            _ => unreachable!(),
+        };
+
+        if let Value::Fun(fun) = method {
+            let mut closure = self.new_env(ev.depth());
+
+            for (index, param) in fun.params.iter().enumerate() {
+                closure.set(param.val(), self.eval_expr(params.get(index).unwrap(), ev));
+            }
+
+            for stmt in fun.body.iter() {
+                self.eval_stmt(stmt, &mut closure);
+            }
+        } else {
+            panic!("unexpected value for function:{:?}", method)
+        }
+        Value::Nil
+    }
+
+    fn eval_get(&mut self, exp: &Expr, ev: &mut Environment) -> Value {
+        Value::Nil
+    }
+
+    fn eval_assign(&mut self, exp: &Expr, ev: &mut Environment) -> Value {
+        Value::Nil
     }
 }
